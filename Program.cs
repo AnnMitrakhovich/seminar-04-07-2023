c3a6321ea987352bdd2cec14d9b3ebbe79c02729
Ann <ann.mitrakhovich@gmail.com>
2023-07-11 13:47:45 +0300

    Home task 60 done without checking

--- Program.cs
+++ Program.cs
@@ -157,87 +157,171 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
-int InputNum(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine()!);
-}
-int[,] Create2DArray(int rows, int columns)
-{
-    return new int[rows, columns];
-}
-void Fill2DArray(int[,] array, int min, int max)
-{
-    Random rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = rnd.Next(min, max + 1);
-}
-void Print2DArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i, j]}\t");
-        Console.WriteLine();
-    }
-}
-int[,] MultiplicationsOf2DArrays(int[,] array1, int[,] array2)
+// int InputNum(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine()!);
+// }
+// int[,] Create2DArray(int rows, int columns)
+// {
+//     return new int[rows, columns];
+// }
+// void Fill2DArray(int[,] array, int min, int max)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = rnd.Next(min, max + 1);
+// }
+// void Print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write($"{array[i, j]}\t");
+//         Console.WriteLine();
+//     }
+// }
+// int[,] MultiplicationsOf2DArrays(int[,] array1, int[,] array2)
 // Массив из сумм элементов умножения строк одного двухмерного массива на элементы столбцов второго двухмерного массива.
 // Умножение возможно только, если количество столбцов первой матрицы равно количеству строк второй матрицы.
-{
-    int[,] multiArray = new int[array1.GetLength(0), array2.GetLength(1)];
-    for (int i = 0; i < array1.GetLength(0); i++)
-    {
-        for (int j = 0; j < array2.GetLength(1); j++)
-        {
-            multiArray[i, j] = 0;
-            for (int x = 0; x < array1.GetLength(1); x++)
-            {
-                multiArray[i, j] += array1[i, x] * array2[x, j];
-            }
-        }
-    }
-    return multiArray;
-}
+// {
+//     int[,] multiArray = new int[array1.GetLength(0), array2.GetLength(1)];
+//     for (int i = 0; i < array1.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array2.GetLength(1); j++)
+//         {
+//             multiArray[i, j] = 0;
+//             for (int x = 0; x < array1.GetLength(1); x++)
+//             {
+//                 multiArray[i, j] += array1[i, x] * array2[x, j];
+//             }
+//         }
+//     }
+//     return multiArray;
+// }
 
-void Check(int[,] array1, int[,] array2, int columns1, int rows2)
-{
-    if (columns1 != rows2) Console.WriteLine("Умножение матриц невозможно.");
-    else
-    {
-        Console.WriteLine("Произведением двух матриц будет: ");
-        int[,] result = MultiplicationsOf2DArrays(array1, array2);
-        Print2DArray(result);
-    }
-}
-int rows1 = InputNum("Введите количество строк для первого массива: ");
-int cols1 = InputNum("Введите количество столбцов для первого массива: ");
-int minValue1 = InputNum("Введите минимальное значение элемента для первого массива: ");
-int maxValue1 = InputNum("Введите максимальное значение элемента для первого массива: ");
-int[,] myArray1 = Create2DArray(rows1, cols1);
-Fill2DArray(myArray1, minValue1, maxValue1);
-Console.WriteLine();
-int rows2 = InputNum("Введите количество строк для второго массива: ");
-int cols2 = InputNum("Введите количество столбцов для второго массива: ");
-int minValue2 = InputNum("Введите минимальное значение элемента для второго массива: ");
-int maxValue2 = InputNum("Введите максимальное значение элемента для второго массива: ");
-int[,] myArray2 = Create2DArray(rows2, cols2);
-Fill2DArray(myArray2, minValue2, maxValue2);
-Print2DArray(myArray1);
-Console.WriteLine();
-Print2DArray(myArray2);
-Console.WriteLine();
-Check(myArray1, myArray2, cols1, rows2);
+// void Check(int[,] array1, int[,] array2, int columns1, int rows2)
+// {
+//     if (columns1 != rows2) Console.WriteLine("Умножение матриц невозможно.");
+//     else
+//     {
+//         Console.WriteLine("Произведением двух матриц будет: ");
+//         int[,] result = MultiplicationsOf2DArrays(array1, array2);
+//         Print2DArray(result);
+//     }
+// }
+// int rows1 = InputNum("Введите количество строк для первого массива: ");
+// int cols1 = InputNum("Введите количество столбцов для первого массива: ");
+// int minValue1 = InputNum("Введите минимальное значение элемента для первого массива: ");
+// int maxValue1 = InputNum("Введите максимальное значение элемента для первого массива: ");
+// int[,] myArray1 = Create2DArray(rows1, cols1);
+// Fill2DArray(myArray1, minValue1, maxValue1);
+// Console.WriteLine();
+// int rows2 = InputNum("Введите количество строк для второго массива: ");
+// int cols2 = InputNum("Введите количество столбцов для второго массива: ");
+// int minValue2 = InputNum("Введите минимальное значение элемента для второго массива: ");
+// int maxValue2 = InputNum("Введите максимальное значение элемента для второго массива: ");
+// int[,] myArray2 = Create2DArray(rows2, cols2);
+// Fill2DArray(myArray2, minValue2, maxValue2);
+// Print2DArray(myArray1);
+// Console.WriteLine();
+// Print2DArray(myArray2);
+// Console.WriteLine();
+// Check(myArray1, myArray2, cols1, rows2);
 
-
-
-// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int[] AvalibleDigits()
+{
+    int[] digits = new int[90];
+    digits[0] = 10;
+    for (int i = 1; i < 90; i++)
+    {
+        digits[i] = digits[i - 1] + 1;
+    }
+    return digits;
+}
+int [] RandomDigits(int [] array1)
+{
+    Random rnd = new Random();
+    int index = 0;
+    int temp = array1[0];
+    for (int i = 0; i < array1.Length/2; i++)
+    {
+        index = rnd.Next(0,90);
+        temp = array1[index];
+        array1[index] = array1[i];
+        array1[i] = temp;
+    }
+    return array1;
+}
+void PrintArray(int[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write(array2[i] + " ");
+    }
+}
+int[,,] Create3DArray(int x, int y, int z)
+{
+    return new int[x, y, z];
+}
+void Fill3DArray(int[,,] array, int[] numbers)
+{
+    int index = 0;
+    if (index < 90)
+    {
+    for (int x = 0; x < array.GetLength(0); x++)
+        for (int y = 0; y < array.GetLength(1); y++)
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+               array[x, y, z] = numbers[index];
+                   index ++;                
+            }
+    }
+    else Console.WriteLine("Далее числа будут повторяться.");
+}
+void Print3DArray(int[,,] array)
+{
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                Console.Write($"{array[x, y, z]} ({x}, {y}, {z})\t");
+            }
+        }
+    }
+
+    Console.WriteLine();
+}
+
+int rows = InputNum("Введите длину: ");
+int cols = InputNum("Введите ширину: ");
+int z = InputNum("Введите глубину: ");
+Console.WriteLine();
+int[] numbersArray = AvalibleDigits();
+PrintArray(numbersArray);
+Console.WriteLine();
+int[] newNumberArray = RandomDigits(numbersArray);
+PrintArray(newNumberArray);
+Console.WriteLine();
+int[,,] myArray = Create3DArray(rows, cols, z);
+Fill3DArray(myArray, newNumberArray);
+Print3DArray(myArray);
+Console.WriteLine();
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
